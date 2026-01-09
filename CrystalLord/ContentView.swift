1db9fe77f3494ba217b0e13353e7246c01ecc5c1
crystal <rystaldoor8656@gmail.com>
2026-01-09 16:45:05 +0800

    Initial Commit

---
+++ CrystalLord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrystalLord
+//
+//  Created by 梁慧晶 on 2026/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrystalLord/CrystalLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrystalLordApp.swift
+//  CrystalLord
+//
+//  Created by 梁慧晶 on 2026/1/9.
+//
+
+import SwiftUI
+
+@main
+struct CrystalLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
